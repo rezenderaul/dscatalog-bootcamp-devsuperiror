fef497255327a0aa60fefa38730d9f0b4fdc665f
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-14 22:17:26 -0300

    DTO

--- backend/src/main/java/com/raulrezende/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/raulrezende/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.raulrezende.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.raulrezende.dscatalog.dto.CategoryDTO;
 import com.raulrezende.dscatalog.entities.Category;
 import com.raulrezende.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
