88a1c242e50d71feaee1c285646ba90985c78de2
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-17 11:58:27 -0300

    Product insert, update, delete

--- backend/src/main/java/com/raulrezende/dscatalog/entities/Product.java
+++ backend/src/main/java/com/raulrezende/dscatalog/entities/Product.java
@@ -51,11 +51,11 @@ public class Product implements Serializable {
 		this.date = date;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
